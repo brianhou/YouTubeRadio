7a9ec6fa494fe13a859cfa576301d2d04044c9e1
Shreyas Chand <shreyas.chand@gmail.com>
2012-09-24 10:04:04 -0700

    added search functionality

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ $(document).ready(function() {
 });
 
 function ytsearch(keyword){
-        var query = $("#search_query").val();
-        var url = "https://gdata.youtube.com/feeds/api/videos?q={0}&alt=json&max-results=5&v=2&orderby=relevance";
+    var query = $("#search_query").val();
+    var url = "https://gdata.youtube.com/feeds/api/videos";
+    var data = {"q":query, "alt":"json", "max-results":"5", "v":"2", "orderby":"relevance"};
+    $.getJSON(url, data, function(data) {
+        var items = [];
+        $.each(data.feed.entry, function(key, val) {
+            $("#search_results").append("<tr><td>" + val.title.$t + "</td></tr>");
+        });
+
+    });
 }
