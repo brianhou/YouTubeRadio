0738a9b3ae8c5a7accd173985b9671261f6fe56b
Shreyas Chand <shreyas.chand@gmail.com>
2012-09-23 17:21:06 -0700

    added search bar content fetching

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#search_button").click(function() {
+        var query = $("#search_query").val();
         $("#video").css("display", "inline");
     });
 });
