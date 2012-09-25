8fd23c03df29220e9ab3194edbfbd5802285c9f3
Marvin Zhang <zhangmarvin95@gmail.com>
2012-09-25 00:17:19 -0700

    fixed empty query

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ $(document).ready(function() {
 
 function ytsearch(keyword){
     var query = $("#search_query").val();
-    var url = "https://gdata.youtube.com/feeds/api/videos";
-    var data = {"q":query, "alt":"json", "max-results":"5", "v":"2", "orderby":"relevance"};
-    $.getJSON(url, data, function(data) {
-        var items = [];
-        $.each(data.feed.entry, function(key, val) {
-		items.push("<tr><td><img src=\"" + val.media$group.media$thumbnail[1].url + "\"/></td><td>" + val.title.$t + "</td></tr>");
-        });
-            $("#search_results").html(items.join());
+    if (query != ""){
+	    var url = "https://gdata.youtube.com/feeds/api/videos";
+	    var data = {"q":query, "alt":"json", "max-results":"5", "v":"2", "orderby":"relevance"};
+	    $.getJSON(url, data, function(data) {
+		var items = [];
+		$.each(data.feed.entry, function(key, val) {
+			items.push("<tr><td><img src=\"" + val.media$group.media$thumbnail[1].url + "\"/></td><td>" + val.title.$t + "</td></tr>");
+		});
+		    $("#search_results").html(items.join());
 
-    });
+	    });
+    }else{
+	    $("#search_results").html("");
+    }
 }
