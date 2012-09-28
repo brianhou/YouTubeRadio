74b47583fb0291caab08882688d59732a2c991f9
brianhou <brian.hou@live.com>
2012-09-28 10:15:32 -0700

    added commas to indicate thousands in the view count

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function secondsToHMS(d){
     var h = Math.floor(d / 3600); var m = Math.floor((d % 3600) / 60); var s = d % 60;
     return ((h > 0 ? h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s);}
 
+function numAddCommas(n){
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(n)){
+	n = n.replace(rgx, '$1' + ',' + '$2');}
+    return n;}
+
 $(document).ready(function() {
     
     $("#search_button").click(ytsearch);
@@ -18,7 +24,7 @@ function ytsearch(keyword){
             $.getJSON(url, data, function(data) {
                 var items = [];
                 $.each(data.feed.entry, function(key, val) {
-                        items.push("<tr><td><a href=\"" + val.content.src + "\"><img src=\"" + val.media$group.media$thumbnail[1].url + "\"/></a></td><td><em>" + val.title.$t + "</em><br>by " + val.author[0].name.$t + "<br>" +  secondsToHMS(val.media$group.yt$duration.seconds) + " | " + val.yt$statistics.viewCount + " views</td></tr>"); // attempting to add hh:mm:ss, but it doesn't necessarily have the right number of digits in mm and ss
+                        items.push("<tr><td><a href=\"" + val.content.src + "\"><img src=\"" + val.media$group.media$thumbnail[1].url + "\"/></a></td><td><em>" + val.title.$t + "</em><br>by " + val.author[0].name.$t + "<br>" +  secondsToHMS(val.media$group.yt$duration.seconds) + " | " + numAddCommas(val.yt$statistics.viewCount) + " views</td></tr>");
                 });
                 $("#search_results").html(items.join());
 
