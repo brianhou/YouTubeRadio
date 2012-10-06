66fa5e8d9059f0c7d3d47dd50d775105e635cfd6
Shreyas Chand <shreyas.chand@gmail.com>
2012-10-05 22:23:29 -0700

    reformatted a bunch of stuff

--- search.js
+++ search.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+    $("#search_button").click(ytsearch);
+    $("#search_query").keyup(ytsearch);
+    //$("#player").css("display", "inline");
+});
+
 function secondsToHMS(d) {
     d = Number(d);
     var h = Math.floor(d / 3600), m = Math.floor((d % 3600) / 60), s = d % 60;
@@ -12,29 +18,23 @@ function numAddCommas(n) {
     return n;
 }
 
-$(document).ready(function () {
-    $("#search_button").click(ytsearch);
-    $("#search_query").keyup(ytsearch);
-    //$("#player").css("display", "inline");
-});
-
 function ytsearch(keyword) {
     var query = $("#search_query").val();
     if (query) {
         var url = "https://gdata.youtube.com/feeds/api/videos";
-	var data = {"q":query, "alt":"json", "max-results":"5", "v":"2", "orderby":"relevance"};
-            $.getJSON(url, data, function (data) {
-                var items = [];
-                $.each(data.feed.entry, function (key, val) {
-                        items.push("<tr><td><a href=\"" + val.content.src + "\">" +
-                                       "<img src=\"" + val.media$group.media$thumbnail[1].url + "\"/></a></td>" +
-                                   "<td><strong>" +  val.title.$t + "</strong>" + 
-                                       "<br>by " + val.author[0].name.$t +
-                                       "<br>" +  secondsToHMS(val.media$group.yt$duration.seconds) +
-                                       " | " + numAddCommas(val.yt$statistics.viewCount) + " views</td></tr>");
-                });
-                $("#search_results").html(items.join());
+        var data = {"q":query, "alt":"json", "max-results":"5", "v":"2", "orderby":"relevance"};
+        $.getJSON(url, data, function (data) {
+            var items = [];
+            $.each(data.feed.entry, function (key, val) {
+                items.push("<tr><td><a href=\"" + val.content.src + "\">" +
+                    "<img src=\"" + val.media$group.media$thumbnail[1].url + "\"/></a></td>" +
+                    "<td><strong>" +  val.title.$t + "</strong>" + 
+                    "<br>by " + val.author[0].name.$t +
+                    "<br>" +  secondsToHMS(val.media$group.yt$duration.seconds) +
+                    " | " + numAddCommas(val.yt$statistics.viewCount) + " views</td></tr>");
             });
+            $("#search_results").html(items.join());
+        });
     }
     else {
         $("#search_results").html("");
