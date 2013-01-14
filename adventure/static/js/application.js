a11ecc2164f4c08278e93ebe1cc8d9140c8b8971
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-14 13:19:58 -0500

    removed errant semicolons

--- adventure/static/js/application.js
+++ adventure/static/js/application.js
@@ -223,7 +223,7 @@ function secondsToHMS(d) {
     d = Number(d);
     var h = Math.floor(d / 3600), m = Math.floor((d % 3600) / 60), s = d % 60;
     return ((h > 0 ? h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s);
-};
+}
 
 function numAddCommas(n) {
     var rgx = /(\d+)(\d{3})/;
@@ -231,4 +231,4 @@ function numAddCommas(n) {
         n = n.replace(rgx, '$1' + ',' + '$2');
     }
     return n;
-};
+}
