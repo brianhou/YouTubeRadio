2ce773d6757a0271c10bdd48e6dadd8f76e9a54b
Shreyas Chand <shreyas.chand@gmail.com>
2013-01-14 04:32:03 -0500

    combined js. keeping two files open was annoying.

--- adventure/static/js/application.js
+++ adventure/static/js/application.js
@@ -93,3 +93,95 @@ function selectVideo(videoID) {
     }
 
 }
+
+var ytplayer;
+function loadYTPlayer(videoID) {
+    nextVideo = videoID;
+    var params = { allowScriptAccess: "always" };
+    var atts = { id: "ytplayer" };
+    // must have a video id, otherwise an error is raised and onYouTubePlayerReady is never called
+    swfobject.embedSWF("http://www.youtube.com/v/{0}?enablejsapi=1&playerapiid=ytplayer&version=3".format(videoID),
+            "ytplayer", "640", "385", "8", null, null, params, atts);
+}
+
+function onYouTubePlayerReady(playerId) {
+    ytplayer = document.getElementById("ytplayer");
+    ytplayer.addEventListener("onStateChange", "playerStateChangeListener");
+    watch(nextVideo);
+}
+
+function playerStateChangeListener(event) {
+    if(event === 0) {
+        watch(nextVideo);
+    }
+}
+
+var nextVideo;
+var watchHistory = [];
+var related = [];
+var oldRelated = [];
+
+// Eventually should reduce stupid suggestions (like exact same video, duplicate music videos, and...?)
+// Check that new video IDs are not in watchHistory
+// A naive way to fix the second would be to check for different titles; not sure how much we can go beyond that.
+
+function watch(videoID) {
+    if (ytplayer) {
+        document.location.hash = videoID;
+        watchHistory.push(videoID);
+        $("#shareLink").val(document.location.href);
+        ytplayer.loadVideoById(videoID);
+
+        var relatedURL = "https://gdata.youtube.com/feeds/api/videos/{0}/related".format(videoID);
+        oldRelated.push(related); // append the previous related list to the oldRelated list
+        getRelated(relatedURL);
+    }
+}
+
+function getRelated(relatedVideosURL) {
+    related.length = 0;
+    var data = {"alt":"json", "v":"2", "key":DEVKEY, "format":"5"};
+    $.getJSON(relatedVideosURL, data, function (data) {
+        $.each(data.feed.entry, function (key, val) {
+            var id = val.media$group.yt$videoid.$t;
+            if (!(id in watchHistory)) {
+                related.push(id);
+            }
+            // val.author[0].name.$t is the uploader
+            // val.gd$rating.average is the score from 1 to 5
+            // val.link[2].href is the related videos feed of the related video (Yes, this is different from the related videos feed of the original video)
+            // val.media$group.media$thumbnail[1].url is the thumbnail
+            // val.media$group.yt$duration
+            // val.media$group.yt$videoid
+            // val.title.$t
+            // val.yt$statistics.viewCount
+            // val.yt$rating.numDislikes and val.yt$rating.numLikes
+        });
+        nextVideo = related.splice(Math.floor(Math.random() * related.length), 1)[0]; //Splice a random item off the list and designate as the next video
+    });
+}
+
+/* Utility Functions */
+
+String.prototype.format = function() {
+    var formatted = this;
+    for (var i=0; i < arguments.length; i++) {
+    	var regexp = new RegExp('\\{'+i+'\\}', 'g');
+    	formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+};
+
+function secondsToHMS(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600), m = Math.floor((d % 3600) / 60), s = d % 60;
+    return ((h > 0 ? h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s);
+};
+
+function numAddCommas(n) {
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(n)) {
+        n = n.replace(rgx, '$1' + ',' + '$2');
+    }
+    return n;
+};
